35857924d327d7288ba769a08db7d7437807138c
chp320 <chp320@naver.com>
2022-05-15 22:03:56 +0900

    /step2/deploy.sh 내 PID 찾을 때 java로 검색할 수 있도록 명령어 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,7 +11,9 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl freelec-springboot2-webservice | grep jar | awk '{print $1}')
+# 정상적으로 grep하지 못함으로 수정함 (jar -> java)
+#CURRENT_PID=$(pgrep -fl freelec-springboot2-webservice | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl freelec-springboot2-webservice | grep java | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
